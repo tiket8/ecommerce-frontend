f32cefe3e50b9c765816004d503539c0ef9a5243
tiket8 <tiket8@tiket8.com>
2024-07-30 14:36:01 -0700

    funcionalidad implementacion carrito y pedido

---
+++ src/app/pedidos/pedidos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PedidoService } from '../../services/pedido.service';
+
+@Component({
+  selector: 'app-pedidos',
+  templateUrl: './pedidos.component.html',
+  styleUrls: ['./pedidos.component.css']
+})
+export class PedidosComponent implements OnInit {
+  pedidos: any[] = [];
+
+  constructor(private pedidoService: PedidoService) {}
+
+  ngOnInit(): void {
+    this.pedidoService.obtenerPedidos().subscribe(data => {
+      this.pedidos = data;
+    });
+  }
+}
