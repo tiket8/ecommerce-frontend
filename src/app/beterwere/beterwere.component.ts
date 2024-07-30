d1beaf43b18264b56ede31f27898f05676fdb589
tiket8 <tiket8@tiket8.com>
2024-07-30 13:17:40 -0700

    se generaron los modulos de inicio electronica beterwere carrito perfil asi como los servicios usuario producto pedido

---
+++ src/app/beterwere/beterwere.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beterwere',
+  standalone: true,
+  imports: [],
+  templateUrl: './beterwere.component.html',
+  styleUrl: './beterwere.component.css'
+})
+export class BeterwereComponent {
+
+}
---
+++ src/app/electronica/electronica.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-electronica',
+  standalone: true,
+  imports: [],
+  templateUrl: './electronica.component.html',
+  styleUrl: './electronica.component.css'
+})
+export class ElectronicaComponent {
+
+}
